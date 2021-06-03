e38eea37311498c7b1f646f7f5df96214c5cbe46
Ahmad <pouramini@gmail.com>
2021-06-03 12:35:31 +0430

    ... Adding run_comet.py

--- run_comet.py
+++ run_comet.py
@@ -209,8 +209,8 @@ class DataTrainingArguments:
     def __post_init__(self):
         if self.dataset_name is None and self.train_file is None and self.validation_file is None:
             raise ValueError("Need either a dataset name or a training/validation file.")
-        #elif self.source_lang is None or self.target_lang is None:
-        #    raise ValueError("Need to specify the source language and the target language.")
+        elif self.source_lang is None or self.target_lang is None:
+            raise ValueError("Need to specify the source language and the target language.")
 
         if self.train_file is not None:
             extension = self.train_file.split(".")[-1]
